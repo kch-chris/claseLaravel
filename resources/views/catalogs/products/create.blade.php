11dc25ea2b834290cffbea2465e26e344bd2e0e1
Christian Murillo <cmurillo@niceandbella.com>
2020-06-09 18:43:56 -0500

    ultimos cambios desktop

--- resources/views/catalogs/products/create.blade.php
+++ resources/views/catalogs/products/create.blade.php
@@ -10,18 +10,24 @@
 
         echo Form::label('Descripcion', null, ['class' => 'control-label']);
         echo Form::text('description', null, array_merge(['class' => 'form-control'], []));
+        echo "<span style='font-color:#F00;' id='errors_name'> ".($errors->first('description')!=null)?$errors->first('description'):""."</span>";
         echo "<br>";
         echo Form::label('Name', null, ['class' => 'control-label']);
         echo Form::text('name', null, array_merge(['class' => 'form-control'], []));
+        echo "<span style='font-color:#F00;' id='errors_name'> ".($errors->first('name')!=null)?$errors->first('name'):""."</span>";
         echo "<br>";
         echo Form::label('Price', null, ['class' => 'control-label']);
         echo Form::number('price',  null, array_merge(['class' => 'form-control','step' => '0.1'], []));
+        echo "<span style='font-color:#F00;' id='errors_name'> ".($errors->first('price')!=null)?$errors->first('price'):""."</span>";
         echo "<br>";
         echo Form::label('Cost', null, ['class' => 'control-label']);
         echo Form::number('cost',  null, array_merge(['class' => 'form-control','step' => '0.1'], []));
+        echo "<span style='font-color:#F00;' id='errors_name'> ".($errors->first('cost')!=null)?$errors->first('cost'):""."</span>";
         echo "<br>";
         echo Form::submit('Save',['class'=>'btn btn-primary']);
 
     echo Form::close();
+      if(isset($errors)) print_r($errors->first('name'));
     @endphp
+    
 @endsection()
--- resources/views/catalogs/products/edit.blade.php
+++ resources/views/catalogs/products/edit.blade.php
@@ -10,15 +10,19 @@
 
         echo Form::label('Descripcion', null, ['class' => 'control-label']);
         echo Form::text('description', $product->description, array_merge(['class' => 'form-control'], []));
+        echo "<span style='color:#F00!important' id='errors_name'> ".($errors->first('description')!=null)?$errors->first('description'):""."</span>";
         echo "<br>";
         echo Form::label('Name', null, ['class' => 'control-label']);
         echo Form::text('name', $product->name, array_merge(['class' => 'form-control'], []));
+        echo "<span style='font-color:#F00;' id='errors_name'> ".($errors->first('name')!=null)?$errors->first('name'):""."</span>";
         echo "<br>";
         echo Form::label('Price', null, ['class' => 'control-label']);
         echo Form::number('price',  $product->price, array_merge(['class' => 'form-control','step' => '0.1'], []));
+        echo "<span style='font-color:#F00;' id='errors_name'> ".($errors->first('price')!=null)?$errors->first('price'):""."</span>";
         echo "<br>";
         echo Form::label('Cost', null, ['class' => 'control-label']);
         echo Form::number('cost',  $product->cost, array_merge(['class' => 'form-control','step' => '0.1'], []));
+        echo "<span style='font-color:#F00!important' id='errors_name'> ".($errors->first('cost')!=null)?$errors->first('cost'):""."</span>";
         echo "<br>";
         echo Form::submit('Save',['class'=>'btn btn-primary']);
 
